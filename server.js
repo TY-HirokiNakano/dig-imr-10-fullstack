776c4b5c8cac5bed957ec88cef79069cd22042b8
TY-HirokiNakano <hirokiiass@gmail.com>
2026-05-27 15:42:11 +0900

    putのエンドポイント実装中

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/api/levels", async (req, res) => {
 });
 
 // 検索や絞り込みの場合はクエリパラメータ、操作の場合はパスパラメータが一般的らしい
-app.put("/api/levels:id", async (req, res) => {
+app.put("/api/levels/:id", async (req, res) => {
   const { id } = req.params;
   await levelRepository.update(id, req.body);
   res.status(204).send();
